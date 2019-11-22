f6f4d0f7eef6217cc19ac88e172cc28bb8de6c0e
james-pope <jamespope@billericak12.com>
2019-11-22 11:30:04 -0500

    initial

--- src/exercises.java
+++ src/exercises.java
@@ -15,7 +15,23 @@ public class exercises {
         }
         return year;
     }
-    publi
+    public static int sumDigits(int n){
+        int count = 1;
+        int sum = 1;
+        while (count < n){
+            count++;
+            sum += count;
+        }
+        return sum;
+    }
+    public static void sillyNumbers(){
+        int i, j;
+        for (i = 3; i < 10; i++){
+            for (j = 1; j < i; j++){
+                System.out.print(j);
+            }
+        }
+    }
 
     public static void main(String [] args){
         System.out.println(addOdds(8));
@@ -24,6 +40,12 @@ public class exercises {
         System.out.println();
 
         System.out.println(howManyYears(120, 150));
+        System.out.println();
 
+        System.out.println(sumDigits(5));
+        System.out.println(sumDigits(8));
+        System.out.println(sumDigits(11));
+
+        sillyNumbers();
     }
 }
