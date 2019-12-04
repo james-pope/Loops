d2858bb23ee0ec55580f8d5a7056e9c83dcfdd28
james-pope <jamespope@billericak12.com>
2019-12-04 12:25:03 -0500

    idke

--- src/Factorial.java
+++ src/Factorial.java
@@ -14,15 +14,33 @@ public class Factorial {
         int n = 1;
         while (e -olde >.001){
             olde = e;
-            e += 1/calcFactorial(n);
+            e += 1/(double)calcFactorial(n);
             n++;
         }
         return e;
+    }
+    public static void calcEx(int x){
+        double e = 1, olde =0;
+        int base =1;
+        int n = 1;
+        double numerator = Math.pow(base, x);
+        while (e - olde >.001){
+           olde = e;
+           e += numerator/ (double)calcFactorial(base)
+        }
+        System.out.printf("e is %2.3f \n", e);
     }
     public static void main(String [] args){
         for (int i = 1; i <=20; i++){   // when testing 1-20 some numbers become negative
             System.out.println(i + " = " + calcFactorial(i));
         }
+        System.out.println();
         System.out.println(calcE());
+        System.out.println();
+
+        for (int j = 1; j <= 5; j++){
+            calcEx(j);
+        }
     }
+
 }
